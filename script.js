d5f4e103ce6f6bf9c3cb7ba37110f582b1e48fb7
= <mukhtarrahimi749@gmail.com>
2024-02-07 21:42:12 +0430

    write the hour start and end js code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let select = document.querySelectorAll("select");
+const select = document.querySelectorAll("select");
+const time = document.querySelector('h1');
 
 for (let i = 23; i >= 0; i--) {
   i = i < 10 ? '0' + i : i  
@@ -9,7 +10,18 @@ for (let i = 23; i >= 0; i--) {
 for(let j = 59; j>=0; j--){
     j = j < 10 ? '0' + j : j;
   let option = `<option value="${j}">${j}</option>`;
-  select[1].firstElementChild.insertAdjacentHTML('afterend', option)
-
-    
+  select[1].firstElementChild.insertAdjacentHTML('afterend', option)  
 }
+
+setInterval(() => {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    h = h < 10 ? '0' + h : h; 
+    m = m < 10 ? '0' + m : m;          
+    s = s < 10 ? '0' + s : s; 
+
+    time.innerHTML = (`${h}:${m}:${s}`);
+}, 1000);
